68b85c6e3076565ecdb1c871881115760b9fd0b4
Ankit <salian.ankit@tcs.com>
2020-01-15 05:46:00 +0530

    Creting CRUD operation for each entity using Spring Data JPA

---
+++ src/main/java/com/ankit/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.ankit.spring5webapp.repositories;
+
+import com.ankit.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/com/ankit/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.ankit.spring5webapp.repositories;
+
+import com.ankit.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
